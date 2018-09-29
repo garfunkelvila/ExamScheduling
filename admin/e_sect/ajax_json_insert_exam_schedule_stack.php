803743492d218a7cba12bc39c10fc28ef5c142b2
Garfunkel <Garfunkel@VILA-PC>
2018-09-30 00:12:20 +0800

    Fixed stuck at pending
    This is because, the stored procedure uses multiple queries.
    get_result()
    Fixed by using multi_query. but now I need to use escape_stings

--- admin/e_sect/ajax_json_insert_exam_schedule_stack.php
+++ admin/e_sect/ajax_json_insert_exam_schedule_stack.php
@@ -1,10 +1,11 @@
 <?php
-	include("../../util_dbHandler.php");
-	include("../../util_validations.php");
+	include_once("../../util_dbHandler.php");
+	include_once("../../util_validations.php");
 	$json = array(
 		'sucess' => false,
 		'result' => 0
 	);
+	
 	$resultArray = array();
 	if($LoggedInAccesID != '1'){
 		echo "STOP!!! ";
@@ -20,27 +21,45 @@
 
 	if(isNotEmpty($_REQUEST['endorseId']) && isNotEmpty($_REQUEST['dayId']) && isNotEmpty($_REQUEST['room'])){
 		$stmt = null;
-		$stmt = $conn->prepare("CALL `insert_exam_schedule_stack`(?,?,?);");
-		$stmt->bind_param('iis',$_REQUEST['endorseId'], $_REQUEST['dayId'], $_REQUEST['room']);
-		$stmt->execute();
-		$sResult = $stmt->get_result();
+		//THIS DONT WORK DUE TO MULTIPLE QUERIES
+		//$stmt = $conn->prepare("CALL `insert_exam_schedule_stack`(?,?,?);");
+		//$stmt->bind_param('iis', $_REQUEST['endorseId'], $_REQUEST['dayId'], $_REQUEST['room']);
+		//$stmt->execute();
+		//$sResult = $stmt->get_result();
+		//$sRow = $stmt->fetch_row();
+		//FIX BUT Needs to sanitize
+		$conn->multi_query("CALL `insert_exam_schedule_stack`(" . $_REQUEST['endorseId'] . ", " . $_REQUEST['dayId'] . ", '" . $_REQUEST['room'] . "');");
+		$conn->next_result();
+		$sResult = $conn->store_result();
 		$sRow = $sResult->fetch_row();
-		#$sRow[0]
+		$sResult->free();
+		
+		//do {
+		// FROM PHP MANUAL MULTI_QUERY
+		//	/* store first result set */
+		//	if ($sResult = $conn->store_result()) {
+		//		$sRow = $sResult->fetch_row()
+//		//		$sResult->free();
+		//	}
+		//	/* print divider */
+		//	/*if ($mysqli->more_results()) {
+		//		printf("-----------------\n");
+		//	}*/
+		//} while ($conn->next_result());
 
 		if ($sRow[0] == 1){
 			$json['sucess'] = true;
-			$json['result'] = "Schedule sucesfully added.";
+			$json['result'] = "Schedule sucesfully added." . $_REQUEST['endorseId'] . ":" . $_REQUEST['dayId'] . ":" . $_REQUEST['room'];
 			echo json_encode($json);
 		}
 		else{
+			$json['result'] = "Nothing added.";
 			if ($sRow[1] == 0)
 				$json['result'] = "Time is too noon, try another room or day";
-			elseif ($sRow[2] == 0)
+			if ($sRow[2] == 0)
 				$json['result'] = "Endorsment already exist";
-			elseif ($sRow[3] == 0)
-				$json['result'] = "Proctor is not available, try another room that uses different time.";
-			else
-				$json['result'] = "Nothing added.";
+			if ($sRow[3] == 0)
+				$json['result'] = "Proctor is not available, try another room that uses different time." . $sRow[0] . $sRow[1] . $sRow[3] . $sRow[3];
 			echo json_encode($json);
 		}
 	}
